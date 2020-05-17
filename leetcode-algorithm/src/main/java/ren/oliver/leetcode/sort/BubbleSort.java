4da25c41f6d679654e00701cc17f07305763525c
oliver-csu <oliver.csu@gmail.com>
2020-05-17 13:02:45 +0800

    冒泡排序

---
+++ leetcode-algorithm/src/main/java/ren/oliver/leetcode/sort/BubbleSort.java
@@ -0,0 +1,110 @@
+package ren.oliver.leetcode.sort;
+
+import java.util.Arrays;
+
+// 冒泡排序
+public class BubbleSort {
+
+    // 冒泡算法的实现
+    public static void bubbleSort(int[] arr) {
+        // 异常判读
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        // 核心算法
+        for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    // 通过异或操作交互
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    // 一个绝对正确的对数器
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // 生成测试样本集合
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // 复制数组
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // 判断两个排序的结果是否一样
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 打印数组的内容
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // 通过对数器对算法进行测试
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+
+}
---
+++ leetcode-app/src/main/java/ren/oliver/leetcode/App.java
@@ -0,0 +1,9 @@
+package ren.oliver.leetcode;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
