11f6fcf1911e7ed06d32f47dfa511da9c2dcde94
oliver-csu <oliver.csu@gmail.com>
2020-05-21 22:21:36 +0800

    优化-巧妙利用短路运算

--- leetcode-algorithm/src/main/java/ren/oliver/leetcode/sort/InsertionSort.java
+++ leetcode-algorithm/src/main/java/ren/oliver/leetcode/sort/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
             return;
         }
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+            for (int j = i - 1; arr[j] > arr[j + 1] && j >= 0; j--) {
                 swap(arr, j, j + 1);
             }
         }
